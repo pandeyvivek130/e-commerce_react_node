771e4402c6329a157db26f9301c99460315b56d7
pandeyvivek130 <pandey.vivek130@gmail.com>
2020-12-26 16:35:55 +0530

    Added Message Box and Loading Box to display on API call

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,20 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Product from '../components/Product'
 import data from '../data';
+import axios from 'axios';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 function HomeScreen() {
+  const[products, setproducts] = useState([]);
+  const[loading, setloading] = useState(false);
+  const[error, setError] = useState(false);
+  useEffect(() => { 
+    const fetchData = async () => {
+      try{
+        setloading(true);
+        const {data} = await axios.get('/api/products');
+        setloading(false);
+        setproducts(data);
+      }
+      catch(err){
+        setError(err.message);
+        setloading(false);
+      }      
+    }
+    fetchData();
+  }, []);
     return (
         <div>
-          <div className="row center">
-            {
-              data.products.map(product => (
-                <Product key={product._id} product={product}></Product>
-              ))
-            }
-            
-          </div>
+          { loading 
+                    ? <LoadingBox></LoadingBox>
+                    : error 
+                            ? <MessageBox variant="danger">{error}</MessageBox>
+                            : (<div className="row center">
+                            {
+                              products.map(product => (
+                                <Product key={product._id} product={product}></Product>
+                              ))
+                            }
+                          </div>)
+          }
         </div>
-    )
-}
+    );
+} 
 
 export default HomeScreen
