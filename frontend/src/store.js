25360f5dc3b32ad1aa5d439453f60f8a3da09580
pandeyvivek130 <pandey.vivek130@gmail.com>
2021-01-13 18:51:34 +0530

    Added Payment and Order screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,7 +11,8 @@ const initialState = {
     cart : {
         cartItems : localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [],
         shippingAddress: localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')): {} 
-    }
+    },
+    paymentMethod : 'PayPal'
 };
 const reducer = combineReducers({
     productList : productListReduer,
